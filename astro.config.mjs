6e83f13333b0c7e48c0ae904e2bf7bab5b83db16
johnnyvaz <johnnyvaz@cod2d.com.br>
2025-01-02 18:57:59 -0300

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions'; // ou '@astrojs/netlify/edge'
 
 export default defineConfig({
-  output: 'server',
+  // Gera site estático (páginas em HTML sem SSR)
+  output: 'static',
   integrations: [tailwind()],
-  adapter: netlify({
-    // Aqui você pode configurar se quiser algo específico
-  }),
+  // Remova o adapter completamente
 });
