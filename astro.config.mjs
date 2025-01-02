77e5eb7192360785dd56b0ce131b3a59d00ed8e6
johnnyvaz <johnnyvaz@cod2d.com.br>
2025-01-02 18:28:05 -0300

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions'; // ou '@astrojs/netlify/edge'
 
 export default defineConfig({
-  integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [tailwind()],
+  adapter: netlify({
+    // Aqui você pode configurar se quiser algo específico
+  }),
 });
