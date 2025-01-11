1386874be1a48e0f39ff532b12e6b5393850f86d
Johnny Vaz consultant <johnnyvaz@cod2d.com.br>
2025-01-11 08:19:36 -0300

    Initial commit from your app

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent1: '#1175D3',
+        accent2: '#A150AD',
+        accent3: '#B72C53',
+        dark: '#261A25',
+        light: '#F7F7F7',
+      },
+      backgroundImage: {
+        'accent-gradient': 'linear-gradient(90deg, #1175D3, #A150AD)',
+      },
+    },
   },
   plugins: [],
 }
